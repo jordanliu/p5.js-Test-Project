6c0b3c357f15c630fd97f03cf4aa289eab07049e
Jordan Liu <jordanxavierliu@gmail.com>
2019-02-16 12:35:52 -0500

    Sliders for sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,38 @@
+let rSlider, gSlider, bSlider;
 
+let fontsize = 40;
 
 function setup() {
-  createCanvas(600,600);
-  background(255);
+  // create canvas
+  createCanvas(600, 600);
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
+
+  // create sliders
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
+  
 }
 
 function draw() {
-  let r1 = random(600);
-  let r2 = random(600);
-  let r3 = random(255);
-  stroke(r3);
-  line(r1, r2, 300, 300);
-
+  const r = rSlider.value();
+  const g = gSlider.value();
+  const b = bSlider.value();
+  fill(r,g,b);
+  noStroke();
+  ellipse(300,300,300,300);
+  textAlign(CENTER);
+  drawWords(width * 0.5,r,g,b);
 }
 
-function mouseClicked(){
-  save('myCanvas.png');
+function drawWords(x,r,g,b) {
+  // The text() function needs three parameters:
+  // the text to draw, the horizontal position,
+  // and the vertical position
+  fill(255);
+  text(r + "," + g +","+ b, x, 300);
 }
