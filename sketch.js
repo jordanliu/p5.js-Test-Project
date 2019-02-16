4ba1b5c766d91c138f84265c79981c745461be9a
Jordan Liu <jordanxavierliu@gmail.com>
2019-02-16 12:05:34 -0500

    Modified sketch.js to use line instead of ellipse

--- sketch.js
+++ sketch.js
@@ -2,12 +2,15 @@
 
 function setup() {
   createCanvas(600,600);
+  background(255);
 }
 
 function draw() {
   let r1 = random(600);
   let r2 = random(600);
-  ellipse(r1, r2, 40, 40);
+  let r3 = random(255);
+  stroke(r3);
+  line(r1, r2, 300, 300);
 
 }
 
